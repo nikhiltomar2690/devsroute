e0f6db9090e543b4abd594de773e5fb71e906cbd
Nikhil <nikhiltomar2690@gmail.com>
2024-04-01 02:34:15 +0530

    mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import { WhyDevsUnite, Home, Roadmaps, Contact } from "./components/pages";
+import { TermsandConditions } from "./components/pages/TermsandConditions";
 import { RoadmapTest } from "./components/pages/RoadmapTest";
 import PageNotFound from "./components/pages/PageNotFound";
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/whydevsunite" element={<WhyDevsUnite />} />
         <Route path="/roadmaps/:roadmapId" element={<RoadmapTest />} />
+        <Route path="/terms" element={<TermsandConditions />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/roadmaps" element={<Roadmaps />} />
         <Route path="*" element={<PageNotFound />} />
