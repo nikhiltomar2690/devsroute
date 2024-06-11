749cb3f00243118d88e30740e47be14d83da4897
Nikhil <nikhiltomar2690@gmail.com>
2024-06-12 00:50:54 +0530

    app-ads.txt file changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      ...copy({
+        targets: [{ src: "app-ads.txt", dest: "dist" }],
+        hook: "writeBundle", // run the plugin after vite build
+      }),
+      enforce: "post",
+    },
+  ],
+});
